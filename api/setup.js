cb28b57d145dfad74fec5cfc4a61831bc2e7ca92
Welton <welton.garcia@sistematic.net.br>
2022-11-15 12:52:10 -0400

    v1

--- api/setup.js
+++ api/setup.js
@@ -9,30 +9,3 @@ app.use(cors())
 app.use(bodyParser())
 app.use(router.routes())
 app.use(router.allowedMethods());
-
-
-
-/* const allowCors = fn => async (ctx) => {
-    ctx.response.setHeader('Access-Control-Allow-Credentials', true)
-    ctx.response.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    ctx.response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    ctx.response.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (ct.method === 'OPTIONS') {
-        ctx.response.status(200).end()
-        return
-    }
-    return await fn(req, res)
-}
-
-const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-}
-
-module.exports = allowCors(handler)
- */
